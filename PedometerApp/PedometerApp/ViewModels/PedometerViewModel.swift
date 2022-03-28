8ee0327d1d5b58e4ff9b24c9b194194dd559d345
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-28 10:26:33 -0400

    added view models foler and pedometer view model to pedometer app

---
+++ PedometerApp/PedometerApp/ViewModels/PedometerViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  PedometerViewModel.swift
+//  PedometerApp
+//
+//  Created by Thomas Cowern New on 3/28/22.
+//  Copyright © 2022 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+import CoreMotion
+
+class PedometerViewModel {
+    
+    var pedometer: CMPedometer
+    
+}
